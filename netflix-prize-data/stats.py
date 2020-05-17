34c3faecdce0997fb5751ca5e1e6343e367408bd
Stiven Cardona <stiven.cardona627@gmail.com>
2020-05-17 12:53:10 -0500

    backup commit

--- netflix-prize-data/stats.py
+++ netflix-prize-data/stats.py
@@ -7,6 +7,25 @@ import math
 import matplotlib.pyplot as plt
 
 storage = {}
+movies = {}
+
+def getValues(points):
+	values = points.split(", ")
+	return (int(values[0]) - 1, int(values[1]))
+
+def getCustomer(arr):
+	if len(arr) > 1:
+		aux = arr[0]
+		aux = aux[1:len(aux)]
+		arr.pop(0)
+		arr.insert(0,aux)
+		aux = arr[len(arr) - 1]
+		aux = aux[0:len(aux) - 2]
+		arr.pop(len(arr) - 1)
+		arr.append(aux)
+	else:
+		arr[0] = arr[0][1:-2]
+	return [ getValues(pair) for pair in arr]
 
 def getBounds(arr):
 	if len(arr) > 1:
@@ -20,7 +39,7 @@ def getBounds(arr):
 		last = first
 	return (first, last)
 
-def graphStats(nameFile, option):
+def graphStats(nameFile, option="reset"):
 	first_movie = 1000000
 	last_movie = 0
 
@@ -31,19 +50,28 @@ def graphStats(nameFile, option):
 			split_line = line.split("), (")
 			key = len(split_line)
 			bounds = getBounds(split_line)
+			if key > 100 and key < 170:
+				print(line[:-1])
 			first_movie = min(bounds[0], first_movie)
 			last_movie = max(bounds[1], last_movie)
 			if key in storage.keys():
 				storage[key] = storage[key] + 1
 			else:
 				storage[key] = 1
+			for pair in getCustomer(split_line):
+				if pair[0] in movies:
+					movies[pair[0]] += 1
+				else:
+					movies[pair[0]] = 1
 
-	print(f"Data from {first_movie} to {last_movie}")
+	if option == "reset":
+		print(f"Data from {first_movie} to {last_movie}")
+		for key in sorted(storage.keys()):
+			print(f"{ storage[key] * 100 / it  }% look {key} movies")
 
-	for key in sorted(storage.keys()):
-		print(f"{ storage[key] * 100 / it  }% look {key} movies")
-
+	print(*reversed(sorted(movies.items())))
+	print(f"{it / 4800}% of the customers look movie from {first_movie} to {last_movie}")
 	plt.scatter(*zip(*storage.items()))
 	plt.show()
 
-graphStats(sys.argv[1])
+graphStats(sys.argv[1], sys.argv[2])
